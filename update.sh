a863652a65244ab986d59f39f94f3ee0717a5391
phoenixbyrd <james.gundlach79@gmail.com>
2021-05-01 22:13:21 -0400

    update update

--- update.sh
+++ update.sh
@@ -2,17 +2,6 @@
 
 git pull origin main
 
-sudo cp youtube2mp3.sh /opt/youtube2mp3/
-sudo cp convert.sh /opt/youtube2mp3/
-sudo cp youtube2mp3.png /opt/youtube2mp3/
 cp Youtube2MP3.desktop ~/.local/share/applications
-
-sudo chown pi:pi /opt/youtube2mp3/
-sudo chown pi:pi /opt/youtube2mp3/youtube2mp3.sh
-sudo chown pi:pi /opt/youtube2mp3/convert.sh
-sudo chown pi:pi /opt/youtube2mp3/youtube2mp3.png
-
-sudo chmod +x /opt/youtube2mp3/youtube2mp3.sh
-sudo chmod +x /opt/youtube2mp3/convert.sh
 
 yad --window-icon=./youtube2mp3.png --title="" --text-align=center --text="Youtube2MP3 Updated!" --button=OK:0 --width=200
